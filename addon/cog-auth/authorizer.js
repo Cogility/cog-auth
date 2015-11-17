3b12f972f05185bdd4b1c7973fa2ce4c72486df5
Michael Latta (TechnoMage) <mlatta@technomage.com>
2015-11-17 10:28:27 -0700

    Update for ember 2.1

--- addon/cog-auth/authorizer.js
+++ addon/cog-auth/authorizer.js
@@ -5,11 +5,11 @@ import Ember from 'ember';
 
 export default Base.extend({
   user: Ember.inject.service('cog-auth'),
-  authorize: function(jqXHR, requestOptions) {
+  authorize: function(sessionData, func) {
     /*jshint unused:vars */
     var userService = this.get('user');
     var token = userService.get('token');
     //console.log('@@@@ Authorizing server request with token '+userService.get('token')+' url: '+requestOptions.url);
-    jqXHR.setRequestHeader('Authorization', token);
+    func('Authorization', token);
   }
 });
